d8421f4af7917120b51bfe933f6d31d3c0f8b362
Adrian Grajeda <agrajeda@pros.com>
2013-11-22 16:14:03 -0400

    Base update code from csv

--- src/org/jala/csvtotable/CsvToTable.java
+++ src/org/jala/csvtotable/CsvToTable.java
@@ -1,56 +1,60 @@
 package org.jala.csvtotable;
 
-import com.mysql.jdbc.Field;
-
 import java.io.*;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.List;
 import java.util.Map;
 
 /**
- * Created with IntelliJ IDEA.
- * User: ale
- * Date: 11/19/13
- * Time: 11:00 PM
- * To change this template use File | Settings | File Templates.
+ * Created with IntelliJ IDEA. User: ale Date: 11/19/13 Time: 11:00 PM To change
+ * this template use File | Settings | File Templates.
  */
 public class CsvToTable {
 
     private String csvFile;
     private String tableName;
-    public List<Map<String, Object>> loadCsvFile(){
+
+    public List<Map<String, Object>> loadCsvFile() {
         File file = new File(csvFile);
         try {
             BufferedReader bufferedReader = new BufferedReader(new FileReader(file));
             String line = bufferedReader.readLine();
-            String [] headers = line.split(",");
+            String[] headers = line.split(",");
             String dml = "INSERT INTO " + tableName + " VALUES(";
-            Field[] dbFields
 
             while ((line = bufferedReader.readLine()) != null) {
-                String [] row = line.split(",");
+                String[] row = line.split(",");
                 StringBuilder fieldQuery = new StringBuilder();
                 for (int i = 0; i < headers.length; i++) {
-                    String fieldName = headers[i];
                     String field = row[i];
-                    com.mysql.jdbc.Field f;
-                    f.
+                    fieldQuery.append("\'");
+                    fieldQuery.append(row[i]);
+                    fieldQuery.append("\',");
+
                 }
-
-
+                fieldQuery.deleteCharAt(fieldQuery.length() - 1);
+                fieldQuery.append(")");
+                Connection connection = DriverManager.getConnection("jdbc:derby://localhost/unitest");
+                Statement update = connection.createStatement();
+                int result = update.executeUpdate(dml.concat(fieldQuery.toString()));
+                System.out.println("updates " + result);
             }
             bufferedReader.close();
         } catch (FileNotFoundException e) {
             e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
         } catch (IOException e) {
             e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
+        } catch (SQLException e) {
+            e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
         }
+        return null;
 
     }
 
-
     public static void main(String[] args) {
-
-
 
     }
 }
