23d7a0d4ed0f6a19b2f40875aa1e5bd7e8640598
Adrian Grajeda <agrajeda@pros.com>
2013-11-26 20:47:26 -0400

    Add integration test with harcoded db query

---
+++ test/org/jala/csvtotable/CsvToTableTest.java
@@ -0,0 +1,51 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package org.jala.csvtotable;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author adrian
+ */
+public class CsvToTableTest {
+    
+
+    /**
+     * Test of loadCsvFile method, of class CsvToTable.
+     */
+    @Test
+    public void testLoadCsvFile() throws SQLException {
+        System.out.println("loadCsvFile");
+        String file = CsvToTable.class.getResource("resources/basic_person_data.csv").getFile();
+        CsvToTable instance = new CsvToTable(file, "test");
+        instance.loadCsvFile();
+        
+        String query = "SELECT * FROM test";
+        Connection con = DriverManager.getConnection("jdbc:derby://localhost:1527/unittest");
+        Statement statement = con.createStatement();
+        ResultSet result = statement.executeQuery(query);
+        int expectedId = 11;
+        String expectedName = "Juan";
+        String expectedLastName = "Perez";
+        String expectedPhone = "123456";
+        result.next();
+        assertEquals(expectedId, result.getInt(1));
+        assertEquals(expectedName, result.getString(2));
+        assertEquals(expectedLastName, result.getString(3));
+        assertEquals(expectedPhone, result.getString(4));
+    }
+
+    
+    
+}
